ebf823f33deee2402ea09e5b00f358fbf98cddfb
久野文菜 <jazzy.kno3.wonder@gmail.com>
2019-12-04 23:15:44 +0900

    Initial Commit

---
+++ Oto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oto
+//
+//  Created by 久野文菜 on 2019/12/04.
+//  Copyright © 2019 kno3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
